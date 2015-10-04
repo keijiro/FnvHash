4150cef2eb064b13e193cfbd0777b5a83a6d2941
Keijiro Takahashi <keijiro@ZX.local>
2015-10-04 14:12:19 +0900

    initial commit

---
+++ Assets/FnvHash/FnvHash.cs
@@ -0,0 +1,37 @@
+﻿namespace FnvHash
+{
+    public static class Hash32
+    {
+        const uint _fnv_prime = 16777619U;
+        const uint _offset_basis = 2166136261U;
+
+        public static uint Compute(uint x)
+        {
+            uint hval = _offset_basis;
+
+            hval *= _fnv_prime;
+            hval ^= x & 0xffU;
+
+            hval *= _fnv_prime;
+            hval ^= (x >> 8) & 0xffU;
+
+            hval *= _fnv_prime;
+            hval ^= (x >> 16) & 0xffU;
+
+            hval *= _fnv_prime;
+            hval ^= (x >> 24) & 0xffU;
+
+            return hval;
+        }
+
+        public static uint Compute(uint x, uint hashSize)
+        {
+            return Compute(x) % hashSize;
+        }
+
+        public static int Compute(int x, int hashSize)
+        {
+            return (int)(Compute((uint)x) % (uint)hashSize);
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        var p = new int[5];
+
+        for (var i = -100; i <= 100; i++)
+            p[FnvHash.Hash32.Compute(i, p.Length)]++;
+
+        foreach (var n in p)
+            Debug.Log(n);
+    }
+}
